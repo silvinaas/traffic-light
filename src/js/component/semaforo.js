4881a5443ae90a423bb48e999430538b423d6bfb
silvinaas <silvinaas4@gmail.com>
2021-09-22 16:58:54 +0000

    avance

--- src/js/component/semaforo.js
+++ src/js/component/semaforo.js
@@ -1,26 +1,19 @@
 import React, { useState } from "react";
 
 const semaforo = () => {
-	const [encendido, setEncendido] = useState();
+	const [encendido, setEncendido] = useState(false);
+	const encenderApagar = () => {
+		setEncendido(!encendido);
+	};
 	return (
 		<div className="container">
 			<div className="palito"></div>
 			<div className="semaforo">
-				<div
-					className={"rojo" + (on == "rojo" ? " prendido" : "")} //aca poner operador ternario:
-					onClick={() => {
-						setEncendido("rojo");
-					}}></div>
+				<div className="rojo" onClick={encenderApagar}></div>
 				<div
 					className="amarillo"
-					onClick={() => {
-						setEncendido("amarillo");
-					}}></div>
-				<div
-					className="verde"
-					onClick={() => {
-						setEncendido("verde");
-					}}></div>
+					onClick={() => setEncendido(true)}></div>
+				<div className="verde" onClick={() => setEncendido(true)}></div>
 			</div>
 		</div>
 	);

8a0838086f868e14c87d992da59a666a04a629d7
silvinaas <silvinaas4@gmail.com>
2021-10-07 20:46:42 +0000

    no solucionado

--- src/js/component/semaforo.js
+++ src/js/component/semaforo.js
@@ -1,21 +1,27 @@
 import React, { useState } from "react";
 
 const semaforo = () => {
-	const [encendido, setEncendido] = useState(false);
-	const encenderApagar = () => {
-		setEncendido(!encendido);
-	};
+	const [encendido, setEncendido] = useState(null);
+	// const encenderApagar = () => {
+	// 	setEncendido(!encendido);
+	// };
+	console.log(encendido);
+	let brillito = "";
+	if ((encendido = "rojo")) brillito = "brillo";
+
 	return (
 		<div className="container">
 			<div className="palito"></div>
 			<div className="semaforo">
-				<div className={"luz rojo" onClick={()encenderApagar}></div>
 				<div
-					className="luz amarillo"
-					onClick={() => setEncendido(true)}></div>
+					className={"luz rojo" + brillito}
+					onClick={() => setEncendido("rojo")}></div>
 				<div
-					className="luz verde"
-					onClick={() => setEncendido(true)}></div>
+					className={"luz amarillo" + brillito}
+					onClick={() => setEncendido("amarillo")}></div>
+				<div
+					className={"luz verde" + brillito}
+					onClick={() => setEncendido("verde")}></div>
 			</div>
 		</div>
 	);

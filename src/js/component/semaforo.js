89b6f78f6486932b0592d62d80c335bef4551ced
silvinaas <silvinaas4@gmail.com>
2021-10-12 01:59:36 +0000

    ready

--- src/js/component/semaforo.js
+++ src/js/component/semaforo.js
@@ -3,25 +3,50 @@ import React, { useState } from "react";
 const semaforo = () => {
 	const [encendido, setEncendido] = useState(null);
 	// const encenderApagar = () => {
-	// 	setEncendido(!encendido);
+	// setEncendido(!encendido);
 	// };
-	console.log(encendido);
-	let brillito = "";
-	if ((encendido = "rojo")) brillito = "brillo";
+	// console.log(encendido);
+	// let brillito = "";
+	// if ((encendido = "rojo")) brillito = "brillo";
+	const [rojoEncendido, setrojoEncendido] = useState(false);
+	const [amarilloEncendido, setamarilloEncendido] = useState(false);
+	const [verdeEncendido, setverdeEncendido] = useState(false);
+
+	const encenderVerde = () => {
+		setverdeEncendido(true);
+		setamarilloEncendido(false);
+		setrojoEncendido(false);
+	};
+	const encenderRojo = () => {
+		setverdeEncendido(false);
+		setamarilloEncendido(false);
+		setrojoEncendido(true);
+	};
+	const encenderAmarillo = () => {
+		setverdeEncendido(false);
+		setamarilloEncendido(true);
+		setrojoEncendido(false);
+	};
 
 	return (
 		<div className="container">
 			<div className="palito"></div>
 			<div className="semaforo">
 				<div
-					className={"luz rojo" + brillito}
-					onClick={() => setEncendido("rojo")}></div>
+					className={rojoEncendido ? "luz rojo brillo" : "luz rojo"}
+					onClick={encenderRojo}></div>
 				<div
-					className={"luz amarillo" + brillito}
-					onClick={() => setEncendido("amarillo")}></div>
+					className={
+						amarilloEncendido
+							? "luz amarillo brillo"
+							: "luz amarillo"
+					}
+					onClick={encenderAmarillo}></div>
 				<div
-					className={"luz verde" + brillito}
-					onClick={() => setEncendido("verde")}></div>
+					className={
+						verdeEncendido ? "luz verde brillo" : "luz verde"
+					}
+					onClick={encenderVerde}></div>
 			</div>
 		</div>
 	);
